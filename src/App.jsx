eeed350c184a0c2137f32504864c3dd0e8c7dd3f
IsakMakimartti <t2mais00@students.oamk.fi>
2026-06-12 18:23:11 +0300

    added condition based rendering for mobile and desktop headers

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,28 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import HeaderMobile from './components/HeaderMobile';
 import HeaderDesktop from './components/HeaderDesktop';
 
 const App = () => {
 
+  const [isMobile, setIsMobile] = useState(false);
+
+  useEffect(() => {
+    function handleResize() {
+      setIsMobile(window.innerWidth <= 768);
+    }
+    handleResize()
+
+    window.addEventListener('resize', handleResize);
+
+    return () => window.removeEventListener('resize', handleResize);
+  }, []);
+
+  if (isMobile)
+    return (
+      <>
+        <HeaderMobile />
+      </>
+    )
   return (
     <>
       <HeaderDesktop />
