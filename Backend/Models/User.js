d243a796feb4331aed3366da8e3251e37c70ea85
miro hannikainen <mirohannikainen@gmail.com>
2026-06-24 17:34:19 +0300

    push origin working login

--- Backend/Models/User.js
+++ Backend/Models/User.js
@@ -2,18 +2,24 @@ const pgPool = require("./Connection.js")
 
 const sql = {
     ADD_USER: 'INSERT INTO "user-details" (username, password, email) values ($1, $2, $3)',
-    GET_PASS: 'SELECT password FROM "user-details" WHERE userid=($1)',
-    GET_USERID: 'SELECT userid FROM "user-details" WHERE username=($1)'
+    GET_PASS: 'SELECT password FROM "user-details" WHERE account_id=($1)',
+    GET_USERID: 'SELECT account_id FROM "user-details" WHERE username=($1)'
 }
 
 async function ADD_USER(username, password){
-    await pgPool.query(sql.ADD_USER, ["Tempuser", "TempUserpass", "Tempusermail"])
+    await pgPool.query(sql.ADD_USER, [username, password, "Tempusermail"])
 }
-async function GETPASS_USER(userid){
-     await pgPool.query(sql.GET_PASS, [userid])
+async function GETPASS_USER(account_id){
+    const results = await pgPool.query(sql.GET_PASS, [account_id])
+     if(results.rowCount !== 0){
+            return results.rows[0].password
+    }
 }   
 async function GETUSER_ID(username) {
-    await pgPool.query(sql.GET_USERID, [username])
+    const results = await pgPool.query(sql.GET_USERID, [username])
+    if(results.rowCount !== 0){
+            return results.rows[0].account_id
+    }
 }
 
 module.exports = {GETUSER_ID, ADD_USER,GETPASS_USER};
--- Backend/Routes/User_Router.js
+++ Backend/Routes/User_Router.js
@@ -5,80 +5,85 @@ const usermodel = require('../Models/User')
 
 // localhost:8080/search/?startdate=xyz&endate=xyz&search=xyz
 // Every post from date range with or without search string
-router.get('/', async(req, res) =>{
- try {
-   res.status(200).json(await usermodel.ADD_USER());
- } catch (err) {
+router.get('/', async (req, res) => {
+  try {
+    res.status(200).json(await usermodel.ADD_USER());
+  } catch (err) {
     res.status(500).json(err.message);
- } 
+  }
 })
 
-router.put('/register', async(req, res) => {
+router.put('/register', async (req, res) => {
   //check items exist
-  if(req.body === undefined || req.body.username === undefined || req.body.password === undefined){
+  if (req.body === undefined || req.body.username === undefined || req.body.password === undefined) {
     res.status(500).json("Wrong format. Missing password or username");
     return
   }
   //perform check before sending
-  if(PassReq(req.body.password)){
+  if (PassReq(req.body.password)) {
     const passHash = await bcrypt.hash(req.body.password, 10)
-    try{
+    try {
       res.status(200).json(await usermodel.ADD_USER(req.body.username, passHash));
-    } catch (err){
+    } catch (err) {
       res.status(500).json(err.message);
     }
+  } else {
+    res.status(500).json("Password requirements not met")
   }
 })
 
 // 
-router.put('/login', async(req, res) => {
-
+router.put('/login', async (req, res) => {
   //check items exist
-  if(!req.body.username || !req.body.password){
+  if (req.body === undefined || req.body.username === undefined || req.body.password === undefined) {
     res.status(500).json("Wrong format. Missing password or username");
     return
   }
-
   //Gets UserId Based on username!
-  const UserId = await usermodel.GETID_USER(req.body.username)
+  const UserId = await usermodel.GETUSER_ID(req.body.username).then(async (userid) => {
+    await usermodel.GETPASS_USER(userid).then(async (HashedPassword) => {
+
+      //Compare passwords
+
+      await bcrypt.compare(req.body.password, HashedPassword).then((Comparison) => {
+        if (Comparison) {
+          //Create Token with secret
+          const token = jwt.sign({
+            userid: userid,
+            expr: "1h"
+          }, process.env.Secret)
+          //add Token
+          res.cookie("jtw", token, {
+            sameSite: "none",
+            secure: true,
+          })
+          res.status(200).json("Login Succesfull");
+        } else {
+          //Wrong pass or username
+          res.status(500).json("Login Failed");
+        }
+      })
+
+
+    })
+
+  })
 
   //Fecth pass for comaparison
-  const HashedPass = await usermodel.GETPASS_USER(UserId)
 
-  //Compare passwords
-  if(bcrypt.compare(req.body.password, HashedPass)){
-
-    //Create Token with secret
-     const token = jwt.sign({
-          username: req.body.username,
-          userid: req.body.userid,
-          expr: "1h"
-        }, process.env.Secret)
-      //add Token
-      response.cookie("jtw", token, {
-        sameSite: "none",
-        secure: true,
-      })
-      
-    res.status(200).json("Success");
-  } else {
-    //Wrong pass or username
-    res.status(500).json("Failed");
-  }
 })
 
-
-
-function PassReq(pass){
-// check for special characters
-const specialChars = `/[!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?]+/;`
-const containsspecial = specialChars.split('').some(char => pass.includes(char))
-//make sure length and special characters are good
-if(pass.toString().length > 8 && containsspecial){
-  return true
-} else {
-  return false
-}
+function PassReq(pass) {
+  console.log("PassReq")
+  // check for special characters
+  const specialChars = `/[!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?]+/;`
+  const containsspecial = specialChars.split('').some(char => pass.includes(char))
+  //make sure length and special characters are good
+  if (pass.toString().length > 8 && containsspecial) {
+    return true
+  } else {
+    return false
+  }
 }
 
 module.exports = router;
