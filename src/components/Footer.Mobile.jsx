2dde4a279bf57934db31202c02d91391a8b3441e
Isak Mäkimartti <t2mais00@students.oamk.fi>
2026-07-03 18:32:17 +0200

    fixed footer button sizing

--- src/components/Footer.Mobile.jsx
+++ src/components/Footer.Mobile.jsx
@@ -5,26 +5,30 @@ import notificationLogo from "../assets/notificationMobile.svg"
 const FooterMobile = () => {
 
     return (
-        <div className="flex flex-row w-full p-4 border-t border-solid border-black rounded-md shadow-sm fixed inset-x-0 bottom-0 bg-white">
+        <div className="flex flex-row w-full p-4 border-t border-solid border-black shadow-sm fixed inset-x-0 bottom-0 bg-white">
             <div className="flex flex-row justify-around w-full">
-                <button className="bg-white text-gray-800">
+                <button
+                    className="flex items-center justify-center bg-white text-gray-800 w-1/3 h-auto"
+                    onClick={() => {
+                        console.log("Add clicked")
+                    }}
+                >
                     <img
                         className='hover:bg-gray-200 rounded-md'
                         src={addLogo}
-                        alt='add'
-                        onClick={() => {
-                            console.log("Add clicked")
-                        }}
+                        alt='addPost'
                     />
                 </button>
-                <button className="bg-white text-gray-800">
+                <button
+                    className="flex items-center justify-center bg-white text-gray-800 w-1/3 h-auto"
+                    onClick={() => {
+                        console.log("Notification clicked")
+                    }}
+                >
                     <img
                         className='hover:bg-gray-200 rounded-md'
                         src={notificationLogo}
                         alt='notification'
-                        onClick={() => {
-                            console.log("Notification clicked")
-                        }}
                     />
                 </button>
             </div>
